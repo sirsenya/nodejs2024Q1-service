cc6e0f833a9cb0b76641c1c3340d82ce50a546e7
Арсений Стефанов <sirsenya@Arsenijs-MacBook-Pro.local>
2024-03-06 13:41:57 +0300

    feat: add album

---
+++ src/album/album.service.ts
@@ -0,0 +1,39 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateAlbumDto } from './dto/create-album.dto';
+import { UpdateAlbumDto } from './dto/update-album.dto';
+import { albums } from 'src/db/in_memory_db';
+import { v4 } from 'uuid';
+import { Album } from './entities/album.entity';
+
+@Injectable()
+export class AlbumService {
+  create(createAlbumDto: CreateAlbumDto) {
+    const newAlbum: Album = new Album({
+      id: v4(),
+      ...createAlbumDto,
+    });
+    albums.push(newAlbum);
+    return newAlbum;
+  }
+
+  findAll() {
+    return albums;
+  }
+
+  findOne(id: string) {
+    const album: Album = albums.find((album: Album) => album.params.id === id);
+    if (!album) throw new NotFoundException('Album Not Found');
+    return album;
+  }
+
+  update(id: string, updateAlbumDto: UpdateAlbumDto) {
+    let album: Album = this.findOne(id);
+    album.params = { ...album.params, ...updateAlbumDto };
+    return album;
+  }
+
+  remove(id: string) {
+    const index = albums.findIndex((album) => album.params.id === id);
+    delete albums[index];
+  }
+}
---
+++ src/album/entities/album.entity.ts
@@ -0,0 +1,10 @@
+export class Album {
+  constructor(public params: IAlbum) {}
+}
+
+interface IAlbum {
+  id: string; // uuid v4
+  name: string;
+  year: number;
+  artistId?: string | null; // refers to Artist
+}
