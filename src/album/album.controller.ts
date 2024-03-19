5f134655f82c6a7cd5d4c215619fc625cf42254e
Арсений Стефанов <sirsenya@Arsenijs-MacBook-Pro.local>
2024-03-19 21:47:17 +0300

    feat: add prisma to album

--- src/album/album.controller.ts
+++ src/album/album.controller.ts
@@ -12,37 +12,42 @@ import {
 import { AlbumService } from './album.service';
 import { CreateAlbumDto } from './dto/create-album.dto';
 import { UpdateAlbumDto } from './dto/update-album.dto';
+import { Album, IAlbum } from './entities/album.entity';
 
 @Controller('album')
 export class AlbumController {
   constructor(private readonly albumService: AlbumService) {}
 
   @Post()
-  create(@Body() createAlbumDto: CreateAlbumDto) {
-    return this.albumService.create(createAlbumDto).params;
+  async create(@Body() createAlbumDto: CreateAlbumDto): Promise<IAlbum> {
+    const album: Album = await this.albumService.create(createAlbumDto);
+    return album.params;
   }
 
   @Get()
-  findAll() {
-    return this.albumService.findAll().map((album) => album.params);
+  async findAll(): Promise<IAlbum[]> {
+    const albums: Album[] = await this.albumService.findAll();
+    return albums.map((album) => album.params);
   }
 
   @Get(':id')
-  findOne(@Param('id', new ParseUUIDPipe()) id: string) {
-    return this.albumService.findOne(id).params;
+  async findOne(@Param('id', new ParseUUIDPipe()) id: string): Promise<IAlbum> {
+    const album: Album = await this.albumService.findOne(id);
+    return album.params;
   }
 
   @Put(':id')
-  update(
+  async update(
     @Param('id', new ParseUUIDPipe()) id: string,
     @Body() updateAlbumDto: UpdateAlbumDto,
-  ) {
-    return this.albumService.update(id, updateAlbumDto).params;
+  ): Promise<IAlbum> {
+    const album: Album = await this.albumService.update(id, updateAlbumDto);
+    return album.params;
   }
 
   @Delete(':id')
   @HttpCode(204)
-  remove(@Param('id', new ParseUUIDPipe()) id: string) {
-    return this.albumService.remove(id);
+  async remove(@Param('id', new ParseUUIDPipe()) id: string): Promise<void> {
+    await this.albumService.remove(id);
   }
 }
